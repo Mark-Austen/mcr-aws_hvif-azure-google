2997bf591d7e0c5b2df66db6a110663a3dd7ee40
Mark Austen <117334224+Mark-Austen@users.noreply.github.com>
2024-08-19 16:48:57 +1000

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,55 +1,55 @@
-resource "aws_vpc" "aws_vpc_sin" {
-  cidr_block = var.aws_vpc_cidr
+resource "aws_vpc" "aws_vpc_1" {
+  cidr_block = var.aws_vpc_1_cidr
   
   tags = {
-    Name = var.aws_vpc_name
+    Name = var.aws_vpc_1_name
   }
 }
 
-resource "aws_subnet" "aws_subnet_sin" {
-  vpc_id                  = aws_vpc.aws_vpc_sin.id
-  cidr_block              = aws_vpc.aws_vpc_sin.cidr_block
+resource "aws_subnet" "aws_subnet_1" {
+  vpc_id                  = aws_vpc.aws_vpc_1.id
+  cidr_block              = aws_vpc.aws_vpc_1.cidr_block
   map_public_ip_on_launch = false
 
   tags = {
-    Name = var.aws_subnet_name
+    Name = var.aws_subnet_1_name
   }
 }
 
-resource "aws_route_table" "aws_route_table_sin" {
-  vpc_id = aws_vpc.aws_vpc_sin.id
+resource "aws_route_table" "aws_route_table_1" {
+  vpc_id = aws_vpc.aws_vpc_1.id
 
   tags = {
-    Name = var.aws_route_table_name
+    Name = var.aws_route_table_1_name
   }
 }
 
-resource "aws_route_table_association" "aws_vpc_sin_route_table_association" {
-  subnet_id      = aws_subnet.aws_subnet_sin.id
-  route_table_id = aws_route_table.aws_route_table_sin.id
+resource "aws_route_table_association" "aws_vpc_1_route_table_association" {
+  subnet_id      = aws_subnet.aws_subnet_1.id
+  route_table_id = aws_route_table.aws_route_table_1.id
 }
 
-resource "aws_vpn_gateway" "aws_vpn_gateway_sin" {
-  vpc_id = aws_vpc.aws_vpc_sin.id
+resource "aws_vpn_gateway" "aws_vpn_gateway_1" {
+  vpc_id = aws_vpc.aws_vpc_1.id
 
   tags = {
-    Name = var.aws_vpn_gateway_name
+    Name = var.aws_vpn_gateway_1_name
   }
 }
 
 resource "aws_vpn_gateway_route_propagation" "vpn_gateway_route_propagation" {
-  vpn_gateway_id = aws_vpn_gateway.aws_vpn_gateway_sin.id
-  route_table_id = aws_route_table.aws_route_table_sin.id
+  vpn_gateway_id = aws_vpn_gateway.aws_vpn_gateway_1.id
+  route_table_id = aws_route_table.aws_route_table_1.id
 }
 
-resource "aws_dx_gateway" "dx_gateway" {
-  name            = var.aws_dx_gateway_name
-  amazon_side_asn = var.aws_dx_gateway_asn
+resource "aws_dx_gateway" "dx_gateway_1" {
+  name            = var.aws_dx_gateway_1_name
+  amazon_side_asn = var.aws_dx_gateway_1_asn
 }
 
 resource "aws_dx_gateway_association" "dx_gateway_association" {
-  dx_gateway_id         = aws_dx_gateway.dx_gateway.id
-  associated_gateway_id = aws_vpn_gateway.aws_vpn_gateway_sin.id
+  dx_gateway_id         = aws_dx_gateway.dx_gateway_1.id
+  associated_gateway_id = aws_vpn_gateway.aws_vpn_gateway_1.id
 }
 
 resource "aws_dx_hosted_private_virtual_interface_accepter" "dx_hosted_private_virtual_interface_accepter" {
